b3cc781fd852e88297117fc18ef598cb23b1e1d3
trehanpraharsh <trehanpraharsh@gmail.com>
2023-09-14 03:21:32 +0530

    Phirse error 😤 ab deploy hogyaa pr https site chahiye isiliye change kraa domain fetch ka iss baar watched vala fetch me dikkat thi

--- src/App.js
+++ src/App.js
@@ -252,7 +252,7 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
         );
         const data = await res.json();
         setMovie(data);
