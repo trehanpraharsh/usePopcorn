ac9448830094aa4f458fd27374b7518dc89cca47
trehanpraharsh <trehanpraharsh@gmail.com>
2023-09-14 03:19:12 +0530

    Phirse error 😤 ab deploy hogyaa pr https site chahiye isiliye change kraa domain fetch ka

--- src/useMovies.js
+++ src/useMovies.js
@@ -20,7 +20,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
