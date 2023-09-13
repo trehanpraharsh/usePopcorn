d769433ed8416b2aff554c996ddcb26fb21529fa
trehanpraharsh <trehanpraharsh@gmail.com>
2023-09-14 03:10:23 +0530

    Fixed average method also, just in case if empty arrays porperties are read

--- src/App.js
+++ src/App.js
@@ -165,7 +165,7 @@ function NumResults({ movies }) {
 }
 
 const average = (arr) =>
-  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+  arr?.reduce((acc, cur, i, arr) => acc + cur / arr?.length, 0);
 
 function Main({ children }) {
   return <main className="main">{children}</main>;
